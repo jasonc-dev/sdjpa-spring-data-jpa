50a2ac989c114fae23210cd344ecc0bb189fc997
jasonc-dev <jasoncohen07@gmail.com>
2022-05-09 16:11:43 +0100

    Implement hibernate Author DAO

---
+++ src/main/java/jason/springframework/jdbc/dao/AuthorDaoHibernate.java
@@ -0,0 +1,68 @@
+package jason.springframework.jdbc.dao;
+
+import jason.springframework.jdbc.domain.Author;
+import org.springframework.data.domain.Pageable;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class AuthorDaoHibernate implements AuthorDao {
+
+    private final EntityManagerFactory emf;
+
+    public AuthorDaoHibernate(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public List<Author> findAllAuthorsByLastName(String lastName, Pageable pageable) {
+        EntityManager em = getEntityManager();
+        try {
+            String hql = "SELECT a FROM Author a WHERE a.lastName = :lastName ";
+
+            if (pageable.getSort().getOrderFor("firstname") != null) {
+                hql = hql + " ORDER BY a.firstName " + pageable.getSort().getOrderFor("firstname")
+                        .getDirection().name();
+            }
+
+            TypedQuery<Author> query = em.createQuery(hql, Author.class);
+            query.setParameter("lastName", lastName);
+            query.setFirstResult(Math.toIntExact(pageable.getOffset()));
+            query.setMaxResults(pageable.getPageSize());
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Author getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Author findAuthorByName(String firstName, String lastName) {
+        return null;
+    }
+
+    @Override
+    public Author saveNewAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public Author updateAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuthorById(Long id) {
+
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+}
---
+++ src/test/java/jason/springframework/jdbc/dao/AuthorDaoHibernateTest.java
@@ -0,0 +1,70 @@
+package jason.springframework.jdbc.dao;
+
+import jason.springframework.jdbc.domain.Author;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"jason.springframework.jdbc.dao"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class AuthorDaoHibernateTest {
+
+        @Autowired
+        EntityManagerFactory entityManagerFactory;
+
+        AuthorDao authorDao;
+
+        @BeforeEach
+        void setUp() {
+            authorDao = new AuthorDaoHibernate(entityManagerFactory);
+        }
+
+        @Test
+        void findAllAuthorsByLastName() {
+            List<Author> authors = authorDao.findAllAuthorsByLastName("Smith", PageRequest.of(0, 10));
+
+            assertThat(authors).isNotNull();
+            assertThat(authors.size()).isEqualTo(10);
+        }
+
+        @Test
+        void findAllAuthorsByLastNameSortLastNameDesc() {
+            List<Author> authors = authorDao.findAllAuthorsByLastName("Smith",
+                    PageRequest.of(0, 10, Sort.by(Sort.Order.desc("firstname"))));
+
+            assertThat(authors).isNotNull();
+            assertThat(authors.size()).isEqualTo(10);
+            assertThat(authors.get(0).getFirstName()).isEqualTo("Yugal");
+        }
+
+        @Test
+        void findAllAuthorsByLastNameSortLastNameAsc() {
+            List<Author> authors = authorDao.findAllAuthorsByLastName("Smith",
+                    PageRequest.of(0, 10, Sort.by(Sort.Order.asc("firstname"))));
+
+            assertThat(authors).isNotNull();
+            assertThat(authors.size()).isEqualTo(10);
+            assertThat(authors.get(0).getFirstName()).isEqualTo("Ahmed");
+        }
+
+        @Test
+        void findAllAuthorsByLastNameAllRecs() {
+            List<Author> authors = authorDao.findAllAuthorsByLastName("Smith", PageRequest.of(0, 100));
+
+            assertThat(authors).isNotNull();
+            assertThat(authors.size()).isEqualTo(40);
+        }
+}
