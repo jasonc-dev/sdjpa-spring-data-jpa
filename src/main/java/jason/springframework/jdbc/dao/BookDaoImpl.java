19b1171df5b5e43db2ec8987e8524b942b6c2ad5
jasonc-dev <jasoncohen07@gmail.com>
2022-05-09 14:12:56 +0100

    find all books using pageable order by title

--- src/main/java/jason/springframework/jdbc/dao/BookDaoImpl.java
+++ src/main/java/jason/springframework/jdbc/dao/BookDaoImpl.java
@@ -19,6 +19,11 @@ public class BookDaoImpl implements BookDao {
     }
 
     @Override
+    public List<Book> findAllBooksSortByTitle(Pageable pageable) {
+        return null;
+    }
+
+    @Override
     public List<Book> findAllBooks(Pageable pageable) {
         return null;
     }
