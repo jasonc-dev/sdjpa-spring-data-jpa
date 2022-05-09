03fe46bf3a887ea666ad5ce36ed95a4c8a92a97b
jasonc-dev <jasoncohen07@gmail.com>
2022-05-09 11:36:35 +0100

    add book DAO

---
+++ src/main/java/jason/springframework/jdbc/dao/BookDao.java
@@ -0,0 +1,17 @@
+package jason.springframework.jdbc.dao;
+
+import jason.springframework.jdbc.domain.Book;
+
+public interface BookDao {
+
+    Book getById(Long id);
+
+    Book findBookByTitle(String title);
+
+    Book saveNewBook(Book book);
+
+    Book updateBook(Book book);
+
+    void deleteBookById(Long id);
+
+}
---
+++ src/main/java/jason/springframework/jdbc/dao/BookDaoImpl.java
@@ -0,0 +1,50 @@
+package jason.springframework.jdbc.dao;
+
+import jason.springframework.jdbc.domain.Book;
+import jason.springframework.jdbc.repositories.BookRepository;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+
+@Component
+public class BookDaoImpl implements BookDao {
+
+    private final BookRepository bookRepository;
+
+    public BookDaoImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book getById(Long id) {
+        return bookRepository.getById(id);
+    }
+
+    @Override
+    public Book findBookByTitle(String title) {
+        return bookRepository.findBookByTitle(title).orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public Book saveNewBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Transactional // run in a single transaction
+    @Override
+    public Book updateBook(Book book) {
+        Book foundBook = bookRepository.getById(book.getId());
+        foundBook.setIsbn(book.getIsbn());
+        foundBook.setPublisher(book.getPublisher());
+        foundBook.setTitle(book.getTitle());
+        foundBook.setAuthorId(book.getAuthorId());
+
+        return bookRepository.save(foundBook);
+    }
+
+    @Override
+    public void deleteBookById(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
