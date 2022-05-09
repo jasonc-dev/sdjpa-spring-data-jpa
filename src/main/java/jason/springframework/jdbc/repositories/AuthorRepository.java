55ed2bd25f37d816dbe57619e3ccfbcf7b0e9a30
jasonc-dev <jasoncohen07@gmail.com>
2022-05-09 11:13:02 +0100

    add query method for find by name

--- src/main/java/jason/springframework/jdbc/repositories/AuthorRepository.java
+++ src/main/java/jason/springframework/jdbc/repositories/AuthorRepository.java
@@ -5,4 +5,6 @@ import jason.springframework.jdbc.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    Author findAuthorByFirstNameAndLastName(String firstName, String lastName);
 }
